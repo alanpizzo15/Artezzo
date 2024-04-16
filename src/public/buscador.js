4c4aff9090566bbe2a1ad612526f8428a27f7c69
alanpizzo15 <63560113+alanpizzo15@users.noreply.github.com>
2024-04-16 16:05:22 -0300

    buscador

--- src/public/buscador.js
+++ src/public/buscador.js
@@ -47,17 +47,38 @@ function funcionBuscar(products, searchListBox){
 const buscadorElmt = document.getElementById('buscador');
 const lupaElmt = document.getElementById('lupaBtn');
 const contenedorElmt = document.getElementById('container');
+const scrollContainerElmt = document.getElementById('searchList');
+const logoBoxElmt = document.getElementById('logoContainer');
+const menuCheckboxElmt = document.getElementById('hamburguerCheck');
+const menuElmt = document.getElementById('hamburguerMenu');
 
-buscadorElmt.classList.add('noDisplay');
+// buscadorElmt.classList.add('noDisplay');
 
 lupaElmt.addEventListener('click', () =>{
-    buscadorElmt.classList.toggle('noDisplay');
-    buscadorElmt.classList.add('search-input');
+
+    console.log('click lupa');
+    // buscadorElmt.classList.toggle('noDisplay');
+    // buscadorElmt.classList.add('search-input');
+
+    logoBoxElmt.classList.toggle('moveToLeft');
+    menuCheckboxElmt.classList.toggle('moveToLeft');
+    menuElmt.classList.toggle('moveToLeft')
 });
 
 contenedorElmt.addEventListener('click', ()=> {
-    console.log('click contenedor');
+    // buscadorElmt.classList.add('noDisplay');
+
+    logoBoxElmt.classList.remove('moveToLeft');
+    menuCheckboxElmt.classList.remove('moveToLeft');
+    menuElmt.classList.remove('moveToLeft');
+});
+
+scrollContainerElmt.addEventListener('click', ()=> {
     buscadorElmt.classList.add('noDisplay');
+
+    logoBoxElmt.classList.toggle('moveToLeft');
+    menuCheckboxElmt.classList.toggle('moveToLeft');
+    menuElmt.classList.toggle('moveToLeft')
 });
 
 
